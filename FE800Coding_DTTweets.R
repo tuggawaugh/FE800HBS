b44b9d2986c07026ab88bb4b991fe07160cecee4
tuggawaugh <tuggawaugh@users.noreply.github.com>
2019-08-08 02:06:55 -0400

    Update FE800Coding_DTTweets.R

--- FE800Coding_DTTweets.R
+++ FE800Coding_DTTweets.R
@@ -1131,4 +1131,20 @@ lmRegressionFit_Afinn_ret_360_Min
 summary(lmRegressionFit_Afinn_ret_360_Min)
 summ(lmRegressionFit_Afinn_ret_360_Min)
 
+lmRegressionFit_Afinn_ret_360_Min
+lmRegressionFit_Afinn_ret_360_Min$df.residual
+names(summary(lmRegressionFit_Afinn_ret_360_Min))
 
+
+lmp <- function (modelobject) {
+  if (class(modelobject) != "lm") stop("Not an object of class 'lm' ")
+  f <- summary(modelobject)$fstatistic
+  p <- pf(f[1],f[2],f[3],lower.tail=F)
+  attributes(p) <- NULL
+  return(p)
+}
+
+lmp(lmRegressionFit_Afinn_ret_360_Min)
+  
+mode(lmRegressionFit_Afinn_ret_360_Min$coefficients)
+colnames()
